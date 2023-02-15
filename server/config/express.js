c2105b4085f36b823ea18e1c1205da0bcb9b9a1a
shahwarxshaista <122122395+shahwarxshaista@users.noreply.github.com>
2023-02-15 15:06:09 -0500

    Add files via upload

---
+++ server/config/express.js
@@ -0,0 +1,68 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+var express = require('express');
+var favicon = require('serve-favicon');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
+var path = require('path');
+var config = require('./environment');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+var config = require('../config/environment');
+var logger = require('./logger');
+
+module.exports = function(app) {
+  var env = app.get('env');
+
+  app.set('config', config);
+  app.set('readerLogger', logger);
+
+  app.use(expressWinston.logger({
+    winstonInstance: logger,
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    //colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
+
+  //app.set('views', config.root + '/server/views');
+  //app.engine('html', require('ejs').renderFile);
+  //app.set('view engine', 'html');
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+
+  var allowCrossDomain = function(req, res, next) {
+      //console.dir(req);
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header('Access-Control-Allow-Headers', 'Authorization');
+
+      next();
+  }
+  app.use(allowCrossDomain);
+
+  if ('prod' === env) {
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', config.root + '/public');
+  }
+
+  if ('dev' === env || 'test' === env) {
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', 'client');
+    app.use(errorHandler()); // Error handler - has to be last
+  }
+};
